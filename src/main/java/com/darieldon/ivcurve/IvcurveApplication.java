25e119f2d43075dd99cad69f79012377294b0ead
DarieldonMedeiros <darieldonbm99@outlook.com>
2026-05-02 16:04:21 -0300

    feat (init): configuração inicial do Projeto Spring Boot com dependências Web, JPA e suporte ao banco SQLite com as configurações do application.yml, application-dev.yml e application-prod.yml

---
+++ src/main/java/com/darieldon/ivcurve/IvcurveApplication.java
@@ -0,0 +1,13 @@
+package com.darieldon.ivcurve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IvcurveApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(IvcurveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/darieldon/ivcurve/IvcurveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darieldon.ivcurve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IvcurveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
